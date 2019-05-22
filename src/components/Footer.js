2dcc619e9d7919c6b3fce72b4eda71074b07683f
Raul Gonzalez <raulg42@gmail.com>
2019-05-22 16:03:46 +0200

    Removed unused dependencies

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => (
   <footer>
     <p>
-      Proudly published with &nbsp;<a href='https://prismic.io' target='_blank' rel='noopener noreferrer'>prismic.io</a>
+      Proudly published with &nbsp;<a href='https://prismic.io' target='_blank' rel='noopener noreferrer'>Prismic</a>
       <br />
       <a href='https://prismic.io' target='_blank' rel='noopener noreferrer'>
         <img className='footer-logo' src='/images/logo-prismic.svg' alt='Gray Prismic logo' />
