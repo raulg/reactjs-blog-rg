789b37630fddd4820fecb48061bda9dd27d8cd1d
Raul Gonzalez <raulg42@gmail.com>
2019-05-22 16:40:16 +0200

    Corrected dumb error

--- src/components/slices/Text.js
+++ src/components/slices/Text.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import { shape, array } from 'prop-types'
 import { RichText } from 'prismic-reactjs'
-import { linkResolver } from '../../prismic-configuration'
 
 const Text = ({ slice, prismicCtx }) => {
   return (
     <div className='post-part single container'>
       <div>
-        {RichText.render(slice.primary.text, linkResolver)}
+        {RichText.render(slice.primary.text, prismicCtx.linkResolver)}
       </div>
     </div>
   )
